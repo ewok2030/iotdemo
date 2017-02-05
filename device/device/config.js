d0fbd7ab9aa540a4e898000c6f2eab1b1b541b1d
ewok2030 <mpalumbo7@gmail.com>
2017-02-05 15:06:15 -0500

    Changed device startup behaviour to download twin

--- device/device/config.js
+++ device/device/config.js
@@ -1,6 +1,5 @@
 const device = {
   connectionString: 'HostName=<host>;DeviceId=<deviceId>;SharedAccessKey=<deviceKey>;',
-  ledPin: '<rpi-pin>',
 };
 
 export default device;
--- device/device/device.js
+++ device/device/device.js
@@ -5,15 +5,14 @@ import { Gpio } from 'onoff';
 /* eslint-disable no-console */
 export default class Device {
 
-  constructor(conStr, pin) {
-    this.pin = pin;
-    this.properties = {};
-
-    // Connection String
+  constructor(conStr) {
+    // Create a client
     this._client = clientFromConnectionString(conStr);
-    // this._interval = setInterval(this.sendMessage, this.properties.interval);
 
-    this._led = Gpio(this.pin, 'out');
+    // Get initial twin properties
+    this.initializeProperties();
+    this._led = Gpio(this.properties.gpio.led, 'out');
+    this.resetInterval();
 
     this._client.on('disconnect', () => {
       clearInterval(this._interval);
@@ -21,62 +20,74 @@ export default class Device {
     });
   }
 
-  connect() {
-    this._client.open((err) => {
+  initializeProperties() {
+    this._client.getTwin((err, twin) => {
       if (err) {
-        console.error(`could not connect to IoT Hub: ${err}`);
-      } else {
-        console.log(`device connected to hub: ${this.hub}`);
-
-        this._client.getTwin((err2, twin) => {
-          if (err2) {
-            console.error('could not get twin');
-          } else {
-            // Create listener for interval property
-            twin.on('properties.desired.interval', (interval) => {
-              // Validate the properties!
-              if (interval < 1000 || interval > 60000) {
-                console.error(`desired transmission interval of ${interval} is not valid (1000 <= x <= 60000). Ignoring.`);
-                return;
-              }
-              console.log(`transmission interval has been updated to ${interval}`);
-              this.properties.interval = interval;
-              this.resetInterval();
-
-              // Report back to the IoT Hub the state of properties
-              twin.properties.reported.update({ interval: this.properties.interval }, (err3) => {
-                if (err3) console.log(`error reporting updated twin: ${err3}`);
-              });
-            });
-
-            // Create listner for transmit property
-            twin.on('properties.desired.transmit', (transmit) => {
-              console.log(`transmission state has been updated to ${transmit}`);
-
-              // If not already in desired state, do something
-              if (this.properties.transmit !== transmit) {
-                this.properties.transmit = transmit;
-                this.resetInterval();
-              }
-
-              // Report back to the IoT Hub the state of properties
-              twin.properties.reported.update({ transmit: this.properties.transmit }, (err3) => {
-                if (err3) console.log(`error reporting updated twin: ${err3}`);
-              });
-            });
-
-            // Create listner for flash property
-            twin.on('properties.desired.flash', (flash) => {
-              console.log(`flash has been updated to ${flash}`);
-              this.properties.flash = flash;
-              // Report back to the IoT Hub the state of properties
-              twin.properties.reported.update({ flash: this.properties.flash }, (err3) => {
-                if (err3) console.log(`error reporting updated twin: ${err3}`);
-              });
-            });
-          }
-        });
+        console.error(`could not get initial device twin: ${err}`);
+        return;
       }
+
+      // Initialize
+      this.properties = {
+        interval: twin.properties.desired.interval,
+        transmit: twin.properties.desired.transmit,
+        flash: twin.properties.desired.flash,
+        gpio: { led: twin.properties.desired.gpio.led, sensor: twin.properties.desired.gpio.sensor },
+      };
+      console.log(`device properties initialized: ${JSON.stringify(this.properties)}`);
+    });
+  }
+
+  connect() {
+    this._client.getTwin((err, twin) => {
+      if (err) {
+        console.error(`could not get twin: ${err}`);
+        return;
+      }
+
+      // Create listener for interval property
+      twin.on('properties.desired.interval', (interval) => {
+        // Validate the properties!
+        if (interval < 1000 || interval > 60000) {
+          console.error(`desired transmission interval of ${interval} is not valid (1000 <= x <= 60000). Ignoring.`);
+          return;
+        }
+
+        console.log(`transmission interval has been updated to ${interval}`);
+        this.properties.interval = interval;
+        this.resetInterval();
+
+        // Report back to the IoT Hub the state of properties
+        twin.properties.reported.update({ interval: this.properties.interval }, (err2) => {
+          if (err2) console.log(`error reporting updated twin: ${err2}`);
+        });
+      });
+
+      // Create listner for transmit property
+      twin.on('properties.desired.transmit', (transmit) => {
+        console.log(`transmission state has been updated to ${transmit}`);
+        // If not already in desired state, do something
+        if (this.properties.transmit !== transmit) {
+          this.properties.transmit = transmit;
+          this.resetInterval();
+        }
+
+        // Report back to the IoT Hub the state of properties
+        twin.properties.reported.update({ transmit: this.properties.transmit }, (err3) => {
+          if (err3) console.log(`error reporting updated twin: ${err3}`);
+        });
+      });
+
+      // Create listner for flash property
+      twin.on('properties.desired.flash', (flash) => {
+        console.log(`flash has been updated to ${flash}`);
+        this.properties.flash = flash;
+
+        // Report back to the IoT Hub the state of properties
+        twin.properties.reported.update({ flash: this.properties.flash }, (err3) => {
+          if (err3) console.log(`error reporting updated twin: ${err3}`);
+        });
+      });
     });
   }
 
