f4e560a1ddeecabae6577d6516ce91e7853ed920
ewok2030 <mpalumbo7@gmail.com>
2017-06-29 16:00:09 -0400

    Added setting to toggle dummy device for testing

--- device/config/config.js
+++ device/config/config.js
@@ -1,25 +1,26 @@
 const config = {
   connectionString: 'HostName=<host>;DeviceId=<deviceId>;SharedAccessKey=<deviceKey>;',
+  dummy: true,
   sensor: {
     type: 22,
-    gpio: 16
+    gpio: 16,
   },
   led: {
     gpio: 19,
-    flashLength: 100
+    flashLength: 100,
   },
   message: {
     interval: {
       min: 1000,
-      max: 3600000
-    }
+      max: 3600000,
+    },
   },
   fileupload: {
     interval: {
       min: 1000,
-      max: 86400000
-    }
-  }
+      max: 86400000,
+    },
+  },
 };
 
 export default config;
