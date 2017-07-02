f9b71676f1b473f90a2196074a24d5c7e4086b1c
ewok2030 <mpalumbo7@gmail.com>
2017-07-02 16:37:51 -0400

    Added configuration environment variables

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
 const config = {
-  hub: '<iothub-host>',
-  messageEndpoint: '<eventhub-endpoint>',
-  messageNamespace: '<iothub-name>',
-  consumerGroup: '<eventhub-consumer-group>',
-  documentdbHost: '<documentdb-url>',
-  documentdbCollectionUri: '<documentdb-collection-uri>',
-  documentdbKey: '<documentdb-key>',
+  hub: process.env.IOTHUB_HOST || '<iothub-host>',
+  messageEndpoint: process.env.MESSAGE_ENDPOINT || '<eventhub-endpoint>',
+  messageNamespace: process.env.MESSAGE_NAMESPACE || '<iothub-name>',
+  messageConsumerGroup: process.env.MESSAGE_CONSUMERGROUP || '<eventhub-consumer-group>',
+  documentdbHost: process.env.DOCUMENTDB_HOST || '<documentdb-url>',
+  documentdbCollectionUri: process.env.DOCUMENTDB_COLLECTION_URI || '<documentdb-collection-uri>',
+  documentdbKey: process.env.DOCUMENTDB_KEY || '<documentdb-key>',
   port: process.env.PORT || 3000,
 };
 export default config;
