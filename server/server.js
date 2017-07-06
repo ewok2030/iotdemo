c550ccec0e0ad9f02c51af94ed3ee0bf540cbde0
ewok2030 <mpalumbo7@gmail.com>
2017-07-06 00:14:23 -0400

    Replaced middleware with webpack-dev-server

--- server/server.js
+++ server/server.js
@@ -8,14 +8,10 @@ import express from 'express';
 import webpack from 'webpack';
 import socketio from 'socket.io';
 // webpack
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config';
+import WebpackDevServer from 'webpack-dev-server';
+import webpackConfig from '../webpack.dev.config';
 import iotSockets from './sockets/sockets';
 import serverConfig from './config';
-
-// Set native promises as mongoose promise
-const isDevelopment = process.env.NODE_ENV === 'development';
 
 /**
  * Create Express server.
@@ -24,55 +20,34 @@ const app = express();
 app.set('port', serverConfig.port);
 app.use(compression());
 app.use(bodyParser.json());
+// default route for single page app
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+/**
+ * Start Express server.
+ */
+/* eslint-disable no-console*/
+if (process.env.NODE_ENV === 'development') {
+  console.log('server is running in development mode');
+
+  const compiler = webpack(webpackConfig);
+  const devServer = new WebpackDevServer(compiler, webpackConfig.devServer);
+  const devPort = 3001;
+  devServer.listen(devPort, () => {
+    console.log(`webpack-dev-server is listening on port ${devPort}`);
+  });
+}
+
 /**
  * Route handlers
  */
 const deviceRoutes = require('./routes/Device.routes');
-
 
 /**
  * API routes
  */
 app.use('/api/device', deviceRoutes.default);
 
-
-/**
- * Start Express server.
- */
-// Enable webpack middleware if in debug mode
-if (isDevelopment) {
-  const compiler = webpack(webpackConfig);
-  const middleware = webpackMiddleware(compiler, {
-    hot: true,
-    inline: true,
-    quiet: true,
-    noInfo: true,
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false,
-    },
-  });
-
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  // TODO: Cant use browserHistory because all routes get redirected here. Also, need to support api
-  app.get('*', (req, res) => {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'public/index.html')));
-    res.end();
-  });
-} else {
-  app.use(express.static(path.join(__dirname, '/public')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
-}
-
-/* eslint-disable*/
 const server = app.listen(serverConfig.port, () => {
   console.log('App is running at http://localhost:%d in %s mode', serverConfig.port, app.get('env'));
   console.log('  Press CTRL-C to stop\n');
@@ -85,5 +60,4 @@ const io = socketio(server);
 app.set('socketio', io);
 iotSockets(io, serverConfig);
 
-/* eslint-enable*/
 export default app;
