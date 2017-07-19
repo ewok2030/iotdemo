804353146be105903489f95ec55c19d73333c3c1
Michael Palumbo <20686457+mpalumbo7@users.noreply.github.com>
2017-07-19 13:37:02 -0400

    Reverted change back to hashHistory in react-router

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
+        <Router history={browserHistory}>
           <Route path="/" component={Layout} >
             <IndexRoute component={DeviceHome} />
             <Route path="/device(/:deviceId)" name="device" component={Device} />
